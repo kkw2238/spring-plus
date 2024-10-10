0b0fed7cb37759428b86813ab2fdf73bf9aeeae5
kkw2238 <kkw2238@naver.com>
2024-10-10 19:31:25 +0900

    fix: Level 2-2
    1. Comment를 가져올 경우
    JOIN이 아닌 JOIN FETCH를 사용하여 불러오게 수정

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // 변경된 코드 : 연관된 데이터를 한 번에 불러오기 위해 JOIN FETCH를 사용하게 수정
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
